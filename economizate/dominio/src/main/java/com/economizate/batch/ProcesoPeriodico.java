a69ebcd893aba5ac5da75a3ca0e3715552c86e61
Angel0108 <riosangelp@gmail.com>
2018-04-27 23:06:37 -0300

    Se elimina codigo no usado en ProcesoPeriodico

--- economizate/dominio/src/main/java/com/economizate/batch/ProcesoPeriodico.java
+++ economizate/dominio/src/main/java/com/economizate/batch/ProcesoPeriodico.java
@@ -1,17 +1,11 @@
 package com.economizate.batch;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.TimerTask;
 
 public class ProcesoPeriodico extends TimerTask{
 	
 	List<IBackup> listaBackups;
-	
-	
-	public ProcesoPeriodico(IBackup... backups) {
-		this.listaBackups = aLista(backups);
-	}
 	
 	public ProcesoPeriodico(List<IBackup> backups) {
 		this.listaBackups = backups;
@@ -20,14 +14,6 @@ public class ProcesoPeriodico extends TimerTask{
 	@Override
 	public void run() {
 		listaBackups.forEach(b -> b.generarBackupMovimientos());
-	}
-	
-	private List<IBackup> aLista(IBackup... backups){
-		List<IBackup> lista = new ArrayList<>();
-		for(IBackup b : backups) {
-			lista.add(b);
-		}
-		return lista;
 	}
 
 }
