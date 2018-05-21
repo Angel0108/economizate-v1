5237c5bb830dc83b3ee21e6dde5f34b6f8f2faed
nicodibi <nicolas_dibiase@yahoo.com.ar>
2018-05-21 16:08:34 -0300

    Se corrige archivo FileDataSource

--- economizate/dominio/src/main/java/com/economizate/servicios/impl/FileDataSource.java
+++ economizate/dominio/src/main/java/com/economizate/servicios/impl/FileDataSource.java
@@ -1,56 +1,42 @@
 package com.economizate.servicios.impl;
 
-import java.io.BufferedOutputStream;
-import java.io.FileNotFoundException;
+import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.OutputStream;
-
-import com.economizate.servicios.BaseReader;
-import com.economizate.servicios.BaseWriter;
 import com.economizate.servicios.DataSource;
-import com.economizate.servicios.FactoryReader;
-import com.economizate.servicios.FactoryWriterMovimientos;
+
 
 public class FileDataSource implements DataSource{
 
-	private OutputStream out;
+	private String name;
 
-	private String nombreArchivo;
-	
-	public FileDataSource(String nombreArchivo) throws FileNotFoundException {
-		
-		this.nombreArchivo = nombreArchivo;
-		
-		out = new FileOutputStream(nombreArchivo);
-		out = new BufferedOutputStream(out); 
-	}
-	
-	@Override
-	public void writeData(String data) {
-				
-		BaseWriter writer = new TXTWriter(nombreArchivo, data);
-		
-		try {
-			writer.write();
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}		
-	}
+    public FileDataSource(String name) {
+        this.name = name;
+    }
 
-	@Override
-	public String readData() {
-		BaseReader reader;
-		try {
-			reader = FactoryReader.getReader(nombreArchivo);
-			return reader.read();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return null;
-		}
-		
-	}
+    @Override
+    public void writeData(String data) {
+        File file = new File(name);
+        try (OutputStream fos = new FileOutputStream(file)) {
+            fos.write(data.getBytes(), 0, data.length());
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    @Override
+    public String readData() {
+        char[] buffer = null;
+        File file = new File(name);
+        try (FileReader reader = new FileReader(file)) {
+            buffer = new char[(int) file.length()];
+            reader.read(buffer);
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return new String(buffer);
+    }
 
 }
