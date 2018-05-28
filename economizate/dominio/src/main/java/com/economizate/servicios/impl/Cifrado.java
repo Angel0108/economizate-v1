ca2879a0954730ba7d1cc0441f3acd6fc9c31d62
nicodibi <nicolas.dibiase22@gmail.com>
2018-05-28 00:09:29 -0300

    Clase cifrado para decorator

--- economizate/dominio/src/main/java/com/economizate/servicios/impl/Cifrado.java
+++ economizate/dominio/src/main/java/com/economizate/servicios/impl/Cifrado.java
@@ -11,18 +11,15 @@ import javax.crypto.Cipher;
 import javax.crypto.CipherOutputStream;
 import javax.crypto.NoSuchPaddingException;
 
-public class Cifrado {
+import com.economizate.servicios.DataSource;
 
-	public Cifrado() throws FileNotFoundException, NoSuchAlgorithmException, NoSuchPaddingException {
-		OutputStream out = new FileOutputStream("somefile");
-		out = new BufferedOutputStream(out); // buffer before writing
-		out = new CipherOutputStream(out,        // encrypt all data
-		         Cipher.getInstance("DES/CBC/PKCS5Padding"));
-		out = new ZipOutputStream(out);      // compress all data
-		//To write our own decorator, we could do:
+public class Cifrado extends DataSourceDecorator {
+
+	public Cifrado(DataSource source) {
+		super(source);
+		// TODO Auto-generated constructor stub
 	}
-	
-	
+
 	
 }
 
