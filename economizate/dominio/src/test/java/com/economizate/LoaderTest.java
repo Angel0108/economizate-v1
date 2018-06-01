3d67db4a8acb5b4ddaf72a32a16fecf24cb13bdf
Angel0108 <riosangelp@gmail.com>
2018-06-01 06:05:59 -0300

    Se agrega test de loader

---
+++ economizate/dominio/src/test/java/com/economizate/LoaderTest.java
@@ -0,0 +1,28 @@
+package com.economizate;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import com.economizate.loader.MyClassLoader;
+import com.economizate.servicios.INube;
+import com.economizate.servicios.impl.Propiedad;
+
+public class LoaderTest {
+	
+	@Test
+	public void cargarClasesDeNubeManagerConExito() throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+		ClassLoader loaderGenerico = MyClassLoader.class.getClassLoader();
+		MyClassLoader loader = new MyClassLoader(loaderGenerico);
+		
+		Class nube = loader.loadClass("ConnectorDrive");
+		loader.loadClass(Propiedad.getInstance().getPropiedad("nubes"));
+		loader.loadClass("NubePropiedades");
+		
+		INube drive = (INube) nube.newInstance();
+		System.out.println(drive.getClass().getName());
+		
+		assertTrue("Clase ConnectorDrive cargada", drive.getClass().getName().equals("com.economizate.nubeManager.ConnectorDrive"));
+	}
+
+}
